5aa4f02df54a352679a85d33d23d9e754ca22ab7
Marei33 <86373668+Marei33@users.noreply.github.com>
2025-10-08 15:12:31 +0000

    Deploying to gh-pages from @ Marei33/marei33.github.io@aba12599220a7c222255764face129133e5dcd76 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Marei33/Marei33.github.io",
+    "data-repo": "Marei33/marei33.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
